6077f479ff746af0688cc76776627483c4cff005
김창대 <cdkim1226@gmail.com>
2021-05-12 02:48:30 +0900

    How to Use Paging in JPA

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -133,4 +133,16 @@ public class MemberRepositoryTest {
             System.out.println("member " + member);
         }
     }
+
+    @Test
+    public void returnType() {
+
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        Optional<Member> aaa = memberRepository.findOptionalByUsername("AAA");
+        System.out.println("aaa = " + aaa.get());
+    }
 }
