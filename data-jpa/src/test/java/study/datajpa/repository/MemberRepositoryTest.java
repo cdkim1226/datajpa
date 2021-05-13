a4e66824b6cd86e822e5fd857e0d490b9757898c
김창대 <cdkim1226@gmail.com>
2021-05-14 07:48:22 +0900

    Use Query Hint & Lock

--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -229,4 +229,30 @@ public class MemberRepositoryTest {
         }
     }
 
+    @Test
+    public void queryHint() {
+
+        Member member1 = new Member("member1",10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        Member findMember = memberRepository.findReadOnlyByUsername("member1");
+        findMember.setUsername("member2");
+
+        em.flush();
+    }
+    @Test
+    public void lock() {
+
+        Member member1 = new Member("member1",10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        List<Member> result = memberRepository.findLockByUsername("member1");
+
+        em.flush();
+    }
+
 }
